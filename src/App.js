5af33365ad4b7962a9c61f4f4d203cc24d363f3c
Bernardo Pozzebon Poluceno <162700391+Bernardo-03P@users.noreply.github.com>
2025-07-08 10:08:18 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Depoimentos from './componentes/depoimentos';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Depoimentos />
+    </div>
+);
+}
+
+export default App;
